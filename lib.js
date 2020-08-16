9fc4167ebc2ff202e8765d67d38758bc1729017c
root <root@DESKTOP-JFF21EO.localdomain>
2020-08-16 20:09:54 +0100

    added page for changing spellings and made randomising not repeat it's self

--- lib.js
+++ lib.js
@@ -1,5 +1,11 @@
 var button;
-
+var spelling;
+var points = 0;
+var input;
+var answer;
+var form;
+var index = 0;
+var dropdown;
 var spellings = [
    'accommodate',
    'accompany',
@@ -14,22 +20,47 @@ var spellings = [
 ];
 
 function init() {
+   input = document.getElementById('answer');
+   form = document.getElementById('form');
    button = document.getElementById('go');
-   console.log(button);
+   dropdown = document.getElementById('dropDown');
+
+   setInterval(loop, 1);
    begin();
 }
 
 function begin() {
-   button.textContent = 'start';
-   console.log(button.value);
+   form.addEventListener('submit', speak);
+
+   shuffle(spellings);
+
+   button.value = 'START';
 }
 
 function speak() {
-   button.textContent = 'next';
+   event.preventDefault();
+   if (button.value != 'START') {
+      answer = input.value;
+      if (answer == spelling) {
+         points++;
+      } else {
+         points--;
+      }
+   }
+   input.value = null;
 
-   var msg = new SpeechSynthesisUtterance(
-      spellings[Math.floor(Math.random() * spellings.length)]
-   );
+   button.value = 'NEXT';
+   spelling = spellings[Math.floor(Math.random() * spellings.length)];
+   var msg = new SpeechSynthesisUtterance(spelling);
    window.speechSynthesis.speak(msg);
-   console.log('spoke');
+}
+
+function shuffle(array) {
+   array.sort(() => Math.random() - 0.5);
+}
+
+function loop() {
+   if (dropdown.options[dropdown.selectedIndex].value == 'Spelling') {
+      window.location = 'spellings.html';
+   }
 }
